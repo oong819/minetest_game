218e1f2353e2618a04fa69cd44c2377b44a50317
Stvk imension <76146430+oong819@users.noreply.github.com>
2022-10-26 00:17:30 +0700

    Update init.lua

--- mods/default/init.lua
+++ mods/default/init.lua
@@ -99,3 +99,24 @@ dofile(default_path.."/legacy.lua")
 		flags = "place_center_x, place_center_z",
 		rotation = "random",
 	})
+
+	minetest.register_decoration({
+		name = "default:calzon",
+		deco_type = "schematic",
+		place_on = {"default:dry_dirt_with_dry_grass"},
+		sidelen = 16,
+		noise_params = {
+			offset = 0.024,
+			scale = 0.015,
+			spread = {x = 250, y = 250, z = 250},
+			seed = 2,
+			octaves = 3,
+			persist = 0.66
+		},
+		biomes = {"savanna"},
+		y_max = 31000,
+		y_min = 1,
+		schematic = minetest.get_modpath("default") .. "/schematics/calmzone.mts",
+		flags = "place_center_x, place_center_z",
+		rotation = "random",
+	})
