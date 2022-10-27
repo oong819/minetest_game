66096fdcb3332f8cddf8b23773e5d79de1ff0642
Stvk imension <76146430+oong819@users.noreply.github.com>
2022-10-27 16:25:37 +0700

    Update init.lua

--- mods/default/init.lua
+++ mods/default/init.lua
@@ -85,9 +85,9 @@ dofile(default_path.."/legacy.lua")
 		place_on = {"default:dirt_with_grass"},
 		sidelen = 16,
 		noise_params = {
-			offset = 0.014,
-			scale = 0.005,
-			spread = {x = 5000, y = 5000, z = 5000},
+			offset = 0.004,
+			scale = 0.001,
+			spread = {x = 128, y = 128, z = 128},
 			seed = 8,
 			octaves = 1,
 			persist = 0.66
@@ -106,9 +106,9 @@ dofile(default_path.."/legacy.lua")
 		place_on = {"default:dry_dirt_with_dry_grass"},
 		sidelen = 16,
 		noise_params = {
-			offset = 0.014,
-			scale = 0.005,
-			spread = {x = 5000, y = 5000, z = 5000},
+			offset = 0.004,
+			scale = 0.001,
+			spread = {x = 128, y = 128, z = 128},
 			seed = 4,
 			octaves = 1,
 			persist = 0.66
