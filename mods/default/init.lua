117d160e990d093db205e1855b39389f2f3b61b0
Stvk imension <76146430+oong819@users.noreply.github.com>
2022-10-29 16:51:54 +0700

    Update init.lua

--- mods/default/init.lua
+++ mods/default/init.lua
@@ -132,3 +132,64 @@ minetest.register_biome({
 		schematic = minetest.get_modpath("default") .. "/schematics/calmzone.mts",
 		flags = "place_center_x, place_center_z",
 	})
+
+minetest.register_decoration({
+		name = "default:outpost",
+		deco_type = "schematic",
+		place_on = {"default:dirt_with_grass"},
+		sidelen = 16,
+		noise_params = {
+			offset = -0.02,
+			scale = 0.001,
+			spread = {x = 5, y = 5, z = 5},
+			seed = 8,
+			octaves = 1,
+			persist = 0.66
+		},
+		biomes = {"deciduous_forest"},
+		y_max = 31000,
+		y_min = 1,
+		schematic = minetest.get_modpath("default") .. "/schematics/outpost.mts",
+		flags = "place_center_x, place_center_z",
+		rotation = "random",
+	})
+
+	minetest.register_decoration({
+		name = "default:storage",
+		deco_type = "schematic",
+		place_on = {"default:dirt_with_rainforest_litter"},
+		sidelen = 16,
+		noise_params = {
+			offset = -0.02,
+			scale = 0.001,
+			spread = {x = 5, y = 5, z = 5},
+			seed = 4,
+			octaves = 1,
+			persist = 0.80
+		},
+		biomes = {"rainforest"},
+		y_max = 31000,
+		y_min = 1,
+		schematic = minetest.get_modpath("default") .. "/schematics/storage.mts",
+		flags = "place_center_x, place_center_z",
+	})
+
+minetest.register_decoration({
+		name = "default:derock",
+		deco_type = "schematic",
+		place_on = {"default:desert_sand"},
+		sidelen = 16,
+		noise_params = {
+			offset = -0.02,
+			scale = 0.001,
+			spread = {x = 5, y = 5, z = 5},
+			seed = 4,
+			octaves = 1,
+			persist = 0.80
+		},
+		biomes = {"deadlydesert"},
+		y_max = 31000,
+		y_min = 1,
+		schematic = minetest.get_modpath("default") .. "/schematics/derock.mts",
+		flags = "place_center_x, place_center_z",
+	})
